056b91b442e0b369fed9cdcc3eecd8eac66f14e7
Raissa0908 <162486832+Raissa0908@users.noreply.github.com>
2024-10-16 13:41:29 -0300

    09/10

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -9,4 +9,12 @@ async function vizualizarInformacoesGlobais() {
     const minutos = Math.round((dados.tempo_medio - horas) * 100)
     const porcentagemConectada = ((pessoasConcetadas / pessoasNoMundo )* 100).toFixed
 
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = 'Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões'
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
+
+vizualizarInformacoesGlobais()
